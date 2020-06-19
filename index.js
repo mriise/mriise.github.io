c39766c1b4bc828303ddbed3f12c8d2039b1bcf9
mriise <mark.riise26@gmail.com>
2020-06-18 20:12:59 -0700

    responsive css design

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 import {html, render} from './src/preact.js'
 
-render(html`<a href="/">Hello!</a>`, document.body);
+
+render(html`
+<div class="profile">
+    <nav>fax</nav>
+    <p>this is dumb</p>
+    <p>so is this</p>
+</div>
+<div class="content"> 
+    <a href="/">Helloo!</a>
+    <footer>feet!</footer>
+
+</div>
+`, document.body);
+
+{/* <i class="fas fa-link"></i> */}
+
